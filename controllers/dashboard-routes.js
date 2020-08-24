31fd26aba3f89de0c810f0c3d5fc30ba7c76a376
davidbelleza <davidbelleza@gmail.com>
2020-08-23 21:47:36 -0500

    add dashboard pages

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -33,7 +33,7 @@ router.get('/', withAuth, (req, res) => {
         .then(dbPostData => {
           // serialize data before passing to template
           const posts = dbPostData.map(post => post.get({ plain: true }));
-          res.render('homepage', { layout: dashboard, posts, loggedIn: true });
+          res.render('dash-home', { layout: "dashboard", posts, loggedIn: true });
         })
         .catch(err => {
           console.log(err);
@@ -43,7 +43,7 @@ router.get('/', withAuth, (req, res) => {
 
 // create new post
 router.get('/new', withAuth, (req,res) => {
-    res.render('new-post', { layout: dashboard });
+    res.render('add-post', { layout: "dashboard" });
 })
 
 // get one post to edit
@@ -77,9 +77,10 @@ router.get('/edit/:id', withAuth, (req,res) => {
             if (dbPostData) {
                 const post = dbPostData.get({ plain: true });
                 
-                res.render('edit-post', { layout: dashboard, post, loggedIn: true });
+                res.render('edit-post', { layout: "dashboard", post, loggedIn: true });
             } else {
-                res.status(404).end();
+                res.status(404).json({message: 'No post found with this id'});
+                return;
             }
         })
         .catch(err => {
