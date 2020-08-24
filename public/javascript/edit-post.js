0a0624d8080f06dbd35295a6a26157953e1cde56
davidbelleza <davidbelleza@gmail.com>
2020-08-23 22:57:15 -0500

    edit login and signup pages

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -2,7 +2,7 @@ async function editFormHandler(event) {
     event.preventDefault();
 
     const title = document.querySelector('input[name="post-title"]').value.trim();
-    const body = document.querySelector('textarea[name="post-body"]').value;
+    const content = document.querySelector('textarea[name="post-content"]').value;
     const id = window.location.toString().split('/')[
         window.location.toString().split('/').length - 1
     ];
@@ -11,7 +11,7 @@ async function editFormHandler(event) {
         method: 'PUT',
         body: JSON.stringify({
           title,
-          body
+          content
         }),
         headers: {
           'Content-Type': 'application/json'
